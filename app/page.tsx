2df5f5df8a4cbc8a693efe27b78f0aae0d8829f1
NguyenTaiNguyen <nguyen.nguyentai@haravan.com>
2023-11-13 01:33:59 +0700

    add meta data

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,13 @@ import Link from 'next/link';
 import styles from '@/app/ui//home.module.css';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
